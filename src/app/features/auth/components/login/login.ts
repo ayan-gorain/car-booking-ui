640c079221bf0788466215ea9bf784e83c71893c
ayan-gorain <goraiayan@email.com>
2026-07-29 21:36:33 +0530

    made

--- src/app/features/auth/components/login/login.ts
+++ src/app/features/auth/components/login/login.ts
@@ -1,13 +1,19 @@
 import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Output } from '@angular/core';
-import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
 import { Router, RouterModule } from '@angular/router';
+import { Main } from '../../../service/main';
 
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ReactiveFormsModule, CommonModule,RouterModule ],
+  imports: [ReactiveFormsModule, CommonModule, RouterModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
@@ -16,10 +22,12 @@ export class Login {
 
   loginForm: FormGroup;
   showLoginPassword = false;
+  loginError: string | null = null;
+  isLoading = false;
 
   constructor(
     private fb: FormBuilder,
-
+    private authService: Main,
     private router: Router
   ) {
     this.loginForm = this.fb.group({
@@ -30,12 +38,41 @@ export class Login {
   }
 
   onLoginSubmit() {
-    if (this.loginForm.valid) {
-      const { email, password } = this.loginForm.value;
-      this.router.navigate(['/dashboard']);
-    } else {
+    if (this.loginForm.invalid) {
       this.loginForm.markAllAsTouched();
+      return;
     }
+
+    this.loginError = null;
+    this.isLoading = true;
+
+    const loginData = {
+      email: this.loginForm.value.email,
+      password: this.loginForm.value.password,
+    };
+
+    this.authService.login(loginData).subscribe({
+      next: (response: any) => {
+        this.isLoading = false;
+
+        console.log('Login Success:', response);
+
+        // Save JWT Token (change "token" if your backend returns another field)
+        if (response.token) {
+          localStorage.setItem('token', response.token);
+        }
+
+        this.router.navigate(['/dashboard']);
+      },
+
+      error: (err: { error: { message: string; }; }) => {
+        this.isLoading = false;
+        console.error(err);
+
+        this.loginError =
+          err.error?.message || 'Invalid email or password.';
+      },
+    });
   }
 
   onSwitchToSignup() {
